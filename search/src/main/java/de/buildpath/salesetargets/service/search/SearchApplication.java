45916512728213c81896296c7942f1d662002f23
Stefan Bley <stefan.bley@saxsys.de>
2016-10-15 11:21:32 +0200

    remove unnecessary annotations and bootstrap.properties

--- search/src/main/java/de/buildpath/salesetargets/service/search/SearchApplication.java
+++ search/src/main/java/de/buildpath/salesetargets/service/search/SearchApplication.java
@@ -1,15 +1,11 @@
 package de.buildpath.salesetargets.service.search;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableAutoConfiguration
-@Configuration
 public class SearchApplication {
 
     public static void main(String[] args) {
--- subscription/src/main/java/de/buildpath/salesetargets/service/subscription/SubscriptionApplication.java
+++ subscription/src/main/java/de/buildpath/salesetargets/service/subscription/SubscriptionApplication.java
@@ -1,15 +1,11 @@
 package de.buildpath.salesetargets.service.subscription;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableAutoConfiguration
-@Configuration
 public class SubscriptionApplication {
 
     public static void main(String[] args) {
